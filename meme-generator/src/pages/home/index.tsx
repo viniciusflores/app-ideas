6932dc1ecd8dd71e3f61b8bfc027c9e8c410d6f4
Vinicius Flores <viniciusflores379@gmail.com>
2020-07-25 23:41:09 +0000

    :beers:

--- meme-generator/src/pages/home/index.tsx
+++ meme-generator/src/pages/home/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import Sketch from 'react-p5'
+import p5Types from 'p5'
 
-import { Container } from './styles'
+import {
+  Container,
+  Header,
+  Body,
+  ImageField,
+  ConfigField,
+  Footer,
+} from './styles'
 
 const Home: React.FC = () => {
-  return <Container>This is home page</Container>
+  let bg: any = null
+  const y: any = 0
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    bg = p5.loadImage('./avatar2.png')
+    p5.createCanvas(216, 233)
+  }
+
+  const draw = (p5: p5Types) => {
+    p5.background(bg)
+    p5.stroke(226, 204, 0)
+    p5.line(0, y, width, y)
+
+    y++
+    if (y > height) {
+      y = 0
+    }
+  }
+
+  return (
+    <Container>
+      <Header>Meme generator</Header>
+      <Body>
+        <ImageField>
+          <Sketch setup={setup} draw={draw} />
+        </ImageField>
+        <ConfigField>top text</ConfigField>
+      </Body>
+      <Footer />
+    </Container>
+  )
 }
 
 export default Home
--- meme-generator/src/pages/home/styles.ts
+++ meme-generator/src/pages/home/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  margin: 0;
+  padding: 0;
+`
+
+export const Header = styled.div``
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const ImageField = styled.div``
+
+export const ConfigField = styled.div``
+
+export const Footer = styled.div``
